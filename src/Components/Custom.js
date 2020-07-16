e34a637c5bd9977297b34ae6654031feaa86fdf1
Ruturaj <ruturaj.ghodke77@gmail.com>
2020-07-17 01:34:36 +0530

    search input changed

--- src/Components/Custom.js
+++ src/Components/Custom.js
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react'
-import { Container, TextField, IconButton } from '@material-ui/core'
+import { Container, TextField, IconButton, Input, InputAdornment } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles'
 import MovieSearchList from './MovieSearchList'
@@ -7,23 +7,24 @@ import TVSearchList from './TVSearchList'
 
 const useStyles = makeStyles((theme) => ({
     iconButton: {
-        border: '2px solid',
         borderColor: theme.palette.primary.main,
         
         transition: '0.3s ease',
-        '&:hover': {
+        '&:hover *': {
             borderColor: theme.palette.text.primary,
+            color: theme.palette.secondary.main
         }
 
     },
     icon: {
-        color: theme.palette.text.primary
+        color: theme.palette.text.primary,
+        transition: '0.4s ease'
     },
     text: {
-        width: '80%',
+        width: '100%',
         '& .MuiOutlinedInput-root': {
             '& fieldset': {
-              borderColor: theme.palette.text.secondary,
+              borderColor: theme.palette.secondary.main,
             }
     }
     },
@@ -43,14 +44,22 @@ export default function Custom(props) {
         <div>
             <Container className={classes.container}>
                 <TextField  label='Search'
-                            color='secondary' 
+                            fullWidth
+                            color='primary'
                             variant='outlined' 
                             className={classes.text}
                             onChange={(e) =>{ setsearchText(e.target.value) 
-                                            setPressed(false)}}  />
-                <IconButton  className={classes.iconButton} onClick={() => setPressed(true)}>
-                    <SearchIcon className={classes.icon} />
-                </IconButton>
+                                            setPressed(false)}}
+                            InputProps={{
+                            endAdornment:
+                                <InputAdornment position='end'>
+                                <IconButton  className={classes.iconButton} onClick={() => setPressed(true)}>
+                                     <SearchIcon className={classes.icon} />
+                                </IconButton>
+                                </InputAdornment>
+                                }}
+                             />
+                
             </Container>
             {props.type==='movie'?<MovieSearchList search={(pressed)?searchText:''} />:<TVSearchList search={(pressed)?searchText:''} />}
             
--- src/Movies.js
+++ src/Movies.js
@@ -3,9 +3,11 @@ import MovieTabs from './Components/MovieTabs'
 
 export default function Movies(props) {
     return (
+        <React.Fragment>
         <div>
             {/* <Typography variant='h3' color='textPrimary'>Movies Page</Typography> */}
             <MovieTabs  />
         </div>
+        </React.Fragment>
     )
 }
